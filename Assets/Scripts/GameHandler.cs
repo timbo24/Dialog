4fc55a1c7a1097014123811a13da093310971df4
Tim <tkkemo@gmail.com>
2020-12-29 11:29:12 -0700

    initializing dialog project

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Fuck Off");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
